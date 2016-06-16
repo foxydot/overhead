add8b54c8d37ae211ffcb75977d15e28449393d6
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-06-16 13:36:03 -0400

    style up those icons

--- assets/themes/wideopen/lib/js/theme-jquery.js
+++ assets/themes/wideopen/lib/js/theme-jquery.js
@@ -21,8 +21,8 @@ jQuery(document).ready(function($) {
 
     if($( window ).width() > 480){
         $(".site-header").sticky();
-        $("nav.nav-primary").sticky({topSpacing:220});
-        $(".notification-bar").sticky({topSpacing:270});
+        $("nav.nav-primary").sticky({topSpacing:220});//140
+        $(".notification-bar").sticky({topSpacing:270});//190
     } else {
         $("nav.nav-primary").sticky({});
     }
