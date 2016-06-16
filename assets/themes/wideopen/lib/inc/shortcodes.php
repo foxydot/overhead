d436d17c032bc3fdca29b81de6d317cab527e92d
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-06-16 11:44:34 -0400

    remove iframe border

--- assets/themes/wideopen/lib/inc/shortcodes.php
+++ assets/themes/wideopen/lib/inc/shortcodes.php
@@ -124,6 +124,6 @@ function msdlab_create_iframe($atts){
         if(!preg_match('%^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu',$url)){
             return 'invalid iframe url';
         }
-        return '<iframe src="'.$url.'" height="'.$height.'" width ="'.$width.'"></iframe>';
+        return '<iframe class="borderless" src="'.$url.'" height="'.$height.'" width ="'.$width.'"></iframe>';
     }
 }
