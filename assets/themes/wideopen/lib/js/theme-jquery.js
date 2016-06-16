57379f4041fc37bf51d037c29fd15c84618d5dec
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-06-16 13:09:56 -0400

    adjust sticky for taller header

--- assets/themes/wideopen/lib/js/theme-jquery.js
+++ assets/themes/wideopen/lib/js/theme-jquery.js
@@ -21,8 +21,8 @@ jQuery(document).ready(function($) {
 
     if($( window ).width() > 480){
         $(".site-header").sticky();
-        $("nav.nav-primary").sticky({topSpacing:140});
-        $(".notification-bar").sticky({topSpacing:190});
+        $("nav.nav-primary").sticky({topSpacing:220});
+        $(".notification-bar").sticky({topSpacing:270});
     } else {
         $("nav.nav-primary").sticky({});
     }
